d9ca1f7acc18bde55ff2ae306c491ef5aa160da0
army8735 <army8735@qq.com>
2017-05-16 14:17:03 +0800

    son

--- app/src/main/java/net/xiguo/test/web/URLs.java
+++ app/src/main/java/net/xiguo/test/web/URLs.java
@@ -6,10 +6,10 @@ package net.xiguo.test.web;
 
 public class URLs {
     public static final String REGISTER_DOMAIN = "http://106.14.223.219:8089/register/";
-    public static final String SEND_REG_SMS = "sendRegSms.htm";
-    public static final String REGISTER_BY_MOBILE = "registerByMobile.htm";
+    public static final String SEND_REG_SMS = "sendRegSms.json";
+    public static final String REGISTER_BY_MOBILE = "registerByMobile.json";
     public static final String LOGIN_DOMAIN = "http://106.14.223.219:8089/login/";
-    public static final String LOGIN_BY_MOBILE = "loginByMobile.htm";
+    public static final String LOGIN_BY_MOBILE = "loginByMobile.json";
 
     public static final String H5_DOMAIN = "http://h5.circling.cc/";
 }
