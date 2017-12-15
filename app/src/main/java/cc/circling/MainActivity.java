814fe59a517d0ddf0e3d50877e64b5fbb09b8d75
army8735 <army8735@qq.com>
2017-12-15 11:08:49 +0800

    虚拟导航栏

--- app/src/main/java/cc/circling/MainActivity.java
+++ app/src/main/java/cc/circling/MainActivity.java
@@ -60,9 +60,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         bgi = findViewById(R.id.bgi);
         progressBar = findViewById(R.id.progressBar);
--- app/src/main/java/cc/circling/X5Activity.java
+++ app/src/main/java/cc/circling/X5Activity.java
@@ -168,9 +168,7 @@ public class X5Activity extends AppCompatActivity {
         }
         AndroidBug5497Workaround.assistActivity(this);
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         titleBar = findViewById(R.id.titleBar);
         title = findViewById(R.id.title);
