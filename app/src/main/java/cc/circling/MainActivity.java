55c34a96feca5a9b282fabe054bbc922256cd55b
army8735 <army8735@qq.com>
2017-12-26 10:32:00 +0800

    update

--- app/src/main/java/cc/circling/MainActivity.java
+++ app/src/main/java/cc/circling/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                         int version = json.getIntValue("version");
                         // 获取本地版本信息
                         SharedPreferences sharedPreferences = getSharedPreferences(PreferenceEnum.H5PACKAGE.name(), MODE_PRIVATE);
-                        final int curVersion = sharedPreferences.getInt("version", 25);
+                        final int curVersion = sharedPreferences.getInt("version", 39);
                         LogUtil.i("checkUpdate version: ", version + ", " + curVersion);
                         if(curVersion < version) {
                             final SharedPreferences.Editor editor = MainActivity.this.getSharedPreferences(PreferenceEnum.H5PACKAGE.name(), Context.MODE_PRIVATE).edit();
