7abb6577470fdaaba88e92b13f83214b5fe082ab
army8735 <army8735@qq.com>
2018-03-11 14:20:39 +0800

    update

--- app/src/main/java/cc/circling/MainActivity.java
+++ app/src/main/java/cc/circling/MainActivity.java
@@ -285,7 +285,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                         }
                         // 获取本地h5版本信息
                         SharedPreferences sharedPreferences = getSharedPreferences(PreferenceEnum.H5PACKAGE.name(), MODE_PRIVATE);
-                        final int curVersion = sharedPreferences.getInt("version", 62);
+                        final int curVersion = sharedPreferences.getInt("version", 63);
                         LogUtil.i("checkUpdate version: ", version + ", " + curVersion);
                         if(curVersion < version) {
                             final SharedPreferences.Editor editor = MainActivity.this.getSharedPreferences(PreferenceEnum.H5PACKAGE.name(), Context.MODE_PRIVATE).edit();
